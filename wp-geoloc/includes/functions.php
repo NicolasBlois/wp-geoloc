305195edb35089bffbaa614f9418c3d346497fa2
Nicolas Blois <nicolas.blois@gmail.com>
2016-01-13 15:43:38 +0100

    Clean GET parameters to avoid XSS and other attacks

--- wp-geoloc/includes/functions.php
+++ wp-geoloc/includes/functions.php
@@ -50,15 +50,15 @@ function wpgeoloc_add_custom_box() {
 }
 
 function wpgeoloc_custom_box() {
-	echo '<input type="hidden" id="geolocation_nonce" name="geolocation_nonce" value="' . wp_create_nonce(plugin_basename(__FILE__) ) . '" />
-    		<label class="screen-reader-text" for="geolocation-address">Geotag</label>
-    		<p>Provide a place for this content</p>
+  echo '<input type="hidden" id="geolocation_nonce" name="geolocation_nonce" value="' . wp_create_nonce(plugin_basename(__FILE__) ) . '" />
+        <label class="screen-reader-text" for="geolocation-address">Geotag</label>
+        <p>Provide a place for this content</p>
         <div id="locationField">
           <input id="autocomplete" name="autocomplete" placeholder="Google place autocomplete" type="text" style="width:100%;" value="'.get_post_meta(get_the_ID(), 'autocomplete',true).'"></input>
           <input class="field" id="latitude" name="latitude" placeholder="latitude" style="width:100%;" value="'.get_post_meta(get_the_ID(), 'latitude',true).'"></input>
           <input class="field" id="longitude" name="longitude" placeholder="longitude" style="width:100%;" value="'.get_post_meta(get_the_ID(), 'longitude',true).'"></input>
         </div>
-	';
+  ';
 }
 
 add_action('save_post', 'wpgeoloc_save_postdata');
@@ -71,19 +71,19 @@ function wpgeoloc_save_postdata($post_id) {
   if('page' == $_POST['post_type'] ) {
 
     if(!current_user_can('edit_page', $post_id))
-		return $post_id;
+      return $post_id;
 
   } else {
 
     if(!current_user_can('edit_post', $post_id))
-		return $post_id;
+      return $post_id;
 
   }
 
   if(isset($_POST['latitude']) && isset($_POST['longitude']) && isset($_POST['autocomplete'])){
     update_post_meta($post_id, 'latitude', $_POST['latitude']);
     update_post_meta($post_id, 'longitude', $_POST['longitude']);
-  	update_post_meta($post_id, 'autocomplete', $_POST['autocomplete']);
+    update_post_meta($post_id, 'autocomplete', $_POST['autocomplete']);
     update_post_meta($post_id, 'wpgeoloc_coords', $_POST['latitude'].",".$_POST['longitude']);
   }
 
@@ -103,20 +103,20 @@ add_action('pre_get_posts','wpgeoloc_alter_query');
 function wpgeoloc_alter_query( $query ){
   global $wp_query, $sc_gds;
   if(isset($_GET['latitude']) && isset($_GET['longitude']) && isset($_GET['distance'])){
-   // Load instance of GeoDataStore
-   if ( ! isset( $sc_gds ) )
-     $sc_gds = new sc_GeoDataStore();
-
-   // Just get the ID's of posts in range
-   $ids = (array) $sc_gds->getPostIDsOfInRange( "post", $_GET['distance'], $_GET['latitude'], $_GET['longitude'] );
-
-   // We we have no results then set an array just one that will trigger no posts found.
-   if( empty( $ids ) )
+    // Load instance of GeoDataStore
+    if ( ! isset( $sc_gds ) )
+      $sc_gds = new sc_GeoDataStore();
+    //Clean GET parameters to avoid XSS and other attacks
+    $distance = preg_replace('/[^-a-zA-Z0-9-.]/', '', $_GET['distance']);
+    $latitude = preg_replace('/[^-a-zA-Z0-9-.]/', '', $_GET['latitude']);
+    $longitude = preg_replace('/[^-a-zA-Z0-9-.]/', '', $_GET['longitude']);
+    // Just get the ID's of posts in range
+    $ids = (array) $sc_gds->getPostIDsOfInRange( "post", $distance, $latitude, $longitude );
+    // We we have no results then set an array just one that will trigger no posts found.
+    if( empty( $ids ) )
      $wp_query->set( 'post__in', array(1) );
-   else
+    else
      $wp_query->set( 'post__in', $ids );
   }
 }
-
-
 ?>
