8f9afa3572984d66c06f9e00d5382fc2538a3467
Nicolas Blois <nicolas.blois@gmail.com>
2016-01-27 20:11:23 +0100

    Update widget-search.php

--- wp-geoloc/includes/widget-search.php
+++ wp-geoloc/includes/widget-search.php
@@ -1,9 +1,22 @@
 <?php
-/**
- * WP Geoloc Search Widget
- * WP Geoloc plugin
- */
- ! defined( 'ABSPATH' ) and exit;
+/*  Copyright 2016  Nicolas Blois  (email : nicolas.blois@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+! defined( 'ABSPATH' ) and exit;
 
 add_action( 'widgets_init', 'wpgeoloc_widgets_init' );
 
@@ -22,7 +35,7 @@ class wpgeoloc_search_widget extends WP_Widget{
 
   function widget($args , $instance) {
 
-  	extract($args);
+    extract($args);
     $title = isset($instance['title']) ? $instance['title'] : esc_html__('Location-based search' , 'wp-geoloc');
 
     echo $before_widget;
