f3f284368d796c7551e51c66cb733c000786f66f
Nicolas Blois <nicolas.blois@gmail.com>
2016-01-27 20:09:34 +0100

    Update settings.php

--- wp-geoloc/includes/settings.php
+++ wp-geoloc/includes/settings.php
@@ -22,36 +22,36 @@
 add_action('admin_menu', 'wpgeoloc_create_menu');
 
 function wpgeoloc_create_menu() {
-	//create new top-level menu
-	add_menu_page('WP Geoloc settings', 'WP Geoloc', 'administrator', __FILE__, 'wpgeoloc_settings_page' , 'dashicons-location' );
-	//call register settings function
-	add_action( 'admin_init', 'register_wpgeoloc_settings' );
+  //create new top-level menu
+  add_menu_page('WP Geoloc settings', 'WP Geoloc', 'administrator', __FILE__, 'wpgeoloc_settings_page' , 'dashicons-location' );
+  //call register settings function
+  add_action( 'admin_init', 'register_wpgeoloc_settings' );
 }
 
 function register_wpgeoloc_settings() {
-	//register our settings
-	register_setting( 'wpgeoloc-settings-group', 'google_api_key' );
+  //register our settings
+  register_setting( 'wpgeoloc-settings-group', 'google_api_key' );
 }
 
 function wpgeoloc_settings_page() {
-	?>
-	<div class="wrap">
-	<h2><?php echo esc_html__( 'WP Geoloc settings', 'wp-geoloc' ); ?></h2>
-	<form method="post" action="options.php">
-		<?php settings_fields( 'wpgeoloc-settings-group' ); ?>
-		<?php do_settings_sections( 'wpgeoloc-settings-group' ); ?>
-		<table class="form-table">
-			<tr valign="top">
-			<th scope="row"><?php echo esc_html__( 'Google Place API KEY', 'wp-geoloc' ); ?></th>
-			<td>
-				<input type="text" name="google_api_key" value="<?php echo esc_attr( get_option('google_api_key') ); ?>" />
-			</td>
-			</tr>
-		</table>
-		<?php submit_button(); ?>
-	</form>
-	</div>
-	<?php
+  ?>
+  <div class="wrap">
+  <h2><?php echo esc_html__( 'WP Geoloc settings', 'wp-geoloc' ); ?></h2>
+  <form method="post" action="options.php">
+    <?php settings_fields( 'wpgeoloc-settings-group' ); ?>
+    <?php do_settings_sections( 'wpgeoloc-settings-group' ); ?>
+    <table class="form-table">
+      <tr valign="top">
+      <th scope="row"><?php echo esc_html__( 'Google Place API KEY', 'wp-geoloc' ); ?></th>
+      <td>
+        <input type="text" name="google_api_key" value="<?php echo esc_attr( get_option('google_api_key') ); ?>" />
+      </td>
+      </tr>
+    </table>
+    <?php submit_button(); ?>
+  </form>
+  </div>
+  <?php
 }
 
 ?>
